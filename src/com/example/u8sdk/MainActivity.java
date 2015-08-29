55b33c80c56638ec4c1ffd21d94717a1b6a2f26e
yesunsong <1042282500@qq.com>
2015-08-29 13:32:57 +0800

    1. 删除res下的values-v11和values-v14 2.修改项目的一些配置，脱离appcompat_v7的依赖，详细见(http://www.mazhuang.org/2014/12/31/simplify-apk/)

--- src/com/example/u8sdk/MainActivity.java
+++ src/com/example/u8sdk/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.u8sdk;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
